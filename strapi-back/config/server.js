85cef0f06ae71dd2488bd8bc07f5f0de778550c7
Artem Pavliuk <art256100@gmail.com>
2020-09-27 22:50:06 +0300

    Basic gatsby + strapi project config

---
+++ strapi-back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '00b1cd9890a6f354768c3fbd11015127'),
+    },
+  },
+});
---
+++ strapi-back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bbc287d8-dfaa-4547-ac54-6badc3c5df0b'
+};
